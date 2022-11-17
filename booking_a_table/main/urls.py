e213a26f486df5becf63ea445e3dfd53438736ed
godofprog <stepanderdichok@gmail.com>
2022-11-17 17:28:02 +0300

    aded empty files of all pages

--- booking_a_table/main/urls.py
+++ booking_a_table/main/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('about', views.about)
+    path('about', views.about),
+    path('menu', views.menu),
+    path('rent_a_table', views.rent_a_table),
+    path('contacts', views.contacts),
+    path('gallery', views.gallery)
 ]
--- booking_a_table/main/views.py
+++ booking_a_table/main/views.py
@@ -7,3 +7,19 @@ def index(request):
 
 def about(request):
     return render(request, 'main/about.html')
+
+
+def menu(request):
+    return render(request, 'main/menu.html')
+
+
+def rent_a_table(request):
+    return render(request, 'main/rent_a_table.html')
+
+
+def contacts(request):
+    return render(request, 'main/contacts.html')
+
+
+def gallery(request):
+    return render(request, 'main/gallery.html')
